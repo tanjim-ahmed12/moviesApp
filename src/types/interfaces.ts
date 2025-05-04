f5b9d0888812f051f2900497c823d0893822632c
healt-tracker-rest <fardin1281@gmail.com>
2025-05-04 04:05:29 +0100

    Movie Details  page components.

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -19,4 +19,11 @@ export interface BaseMovieProps {
   export interface BaseMovieListProps { 
     movies: BaseMovieProps[];
   }    
+  export interface MovieDetailsProps extends BaseMovieProps {
+    genres: {
+      id: number;
+      name: string;
+    }[];
+  }
+
 
