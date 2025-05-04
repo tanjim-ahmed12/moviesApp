5404c45ae1fde43befed147e1924990a4804f2aa
healt-tracker-rest <fardin1281@gmail.com>
2025-05-04 04:39:02 +0100

    Movie Details Page rendering.

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -25,5 +25,20 @@ export interface BaseMovieProps {
       name: string;
     }[];
   }
+  export interface MovieImage {
+    file_path: string;
+    aspect_ratio?: number; //some props are optional...
+    height?: number;
+    iso_639_1?: string;
+    vote_average?: number;
+    vote_count?: number;
+    width?: number;
+  }
+  
+  export interface MoviePageProps {
+    movie: MovieDetailsProps;
+    images: MovieImage[];
+  }
+  
 
 
