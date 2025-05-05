d4c389f71c61f370256beaa147d58e4ebda30577
healt-tracker-rest <fardin1281@gmail.com>
2025-05-05 04:45:59 +0100

    Exercise - Show favourite icon on Movie Details page..

--- src/components/headerMovie/index.tsx
+++ src/components/headerMovie/index.tsx
@@ -5,10 +5,11 @@ import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
-import { MovieDetailsProps } from "../../types/interfaces"; 
+import FavoriteIcon from "@mui/icons-material/Favorite"; // Import red heart
+import { MovieDetailsProps } from "../../types/interfaces";
 
 const styles = {
-    root: {  
+  root: {
     display: "flex",
     justifyContent: "space-around",
     alignItems: "center",
@@ -18,7 +19,12 @@ const styles = {
 };
 
 const MovieHeader: React.FC<MovieDetailsProps> = (movie) => {
-  
+  //  Read favourites from localStorage
+  const favourites = JSON.parse(localStorage.getItem("favourites") || "[]");
+
+  //  Check if this movie is in favourites
+  const isFavourite = favourites.some((fav: { id: number }) => fav.id === movie.id);
+
   return (
     <Paper component="div" sx={styles.root}>
       <IconButton aria-label="go back">
@@ -26,13 +32,18 @@ const MovieHeader: React.FC<MovieDetailsProps> = (movie) => {
       </IconButton>
 
       <Typography variant="h4" component="h3">
-        {movie.title}{"   "}
+        {movie.title}{" "}
+        {isFavourite && ( //  Conditionally show red heart
+          <FavoriteIcon sx={{ color: "red", verticalAlign: "middle" }} />
+        )}
+        {" "}
         <a href={movie.homepage}>
-          <HomeIcon color="primary"  fontSize="large"/>
+          <HomeIcon color="primary" fontSize="large" />
         </a>
         <br />
-        <span>{`${movie.tagline}`} </span>
+        <span>{movie.tagline}</span>
       </Typography>
+
       <IconButton aria-label="go forward">
         <ArrowForwardIcon color="primary" fontSize="large" />
       </IconButton>
