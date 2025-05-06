d3df1a81fcf028e0a9f316b2c579cc8a63075fe3
healt-tracker-rest <fardin1281@gmail.com>
2025-05-06 17:50:27 +0100

    Refactor: Favourite movie list now stored in a context.

--- src/components/movieCard/index.tsx
+++ src/components/movieCard/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {MouseEvent, useContext} from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
@@ -15,6 +15,7 @@ import img from '../../images/film-poster-placeholder.png';
 import { BaseMovieProps } from "../../types/interfaces"; 
 import { Link } from "react-router-dom";
 import Avatar from "@mui/material/Avatar";
+import { MoviesContext } from "../../contexts/movieContext";
 
 const styles = {
   card: { maxWidth: 345 },
@@ -27,21 +28,26 @@ const styles = {
 
 interface MovieCardProps  {
   movie: BaseMovieProps;
-  selectFavourite: (movieId: number) => void;
+  // selectFavourite: (movieId: number) => void;
 }
 
-const MovieCard: React.FC<MovieCardProps> = ({ movie, selectFavourite }) => {
+const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
  
-  const handleAddToFavourite = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    selectFavourite(movie.id);
-  };
+  const { favourites, addToFavourites } = useContext(MoviesContext);//NEW
+
+  const isFavourite = favourites.find((id) => id === movie.id)? true : false;//NEW
+   
+    const handleAddToFavourite = (e: MouseEvent<HTMLButtonElement>) => {//NEW
+      e.preventDefault();
+      addToFavourites(movie);
+    };
+  
 
   return (
     <Card sx={styles.card}>
       <CardHeader
         avatar={
-          movie.favourite ? (
+          isFavourite ? (
             <Avatar sx={styles.avatar}>
               <FavoriteIcon />
             </Avatar>
--- src/index.tsx
+++ src/index.tsx
@@ -7,34 +7,28 @@ import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader'
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools';
+import MoviesContextProvider from "./contexts/movieContext";
 
 import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
 
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <SiteHeader />      {/* New Header  */}
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/movies/favourites">Favourites</Link>
-            </li>
-          </ul>
-          <Routes>
-            <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
-            <Route path="/movies/:id" element={<MoviePage />} />
-            <Route path="/" element={<HomePage />} />
-            <Route path="*" element={<Navigate to="/" />} />
-            <Route path="/reviews/:id" element={<MovieReviewPage/>} />
-            <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
-          </Routes>
-      </BrowserRouter>
-      <ReactQueryDevtools initialIsOpen={false}/>
-    </QueryClientProvider>
+   <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+    <SiteHeader/>
+      <MoviesContextProvider>
+        <Routes>
+          <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
+          <Route path="/movies/:id" element={<MoviePage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<Navigate to="/" />} />
+          <Route path="/reviews/:id" element={<MovieReviewPage/>} />
+          <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
+        </Routes>
+      </MoviesContextProvider>
+    </BrowserRouter>
+   </QueryClientProvider>
   );
 };
 
